0c5bb74274c36c2f41cd931e97e521949578df13
Joekahora <gachau.joel@gmail.com>
2021-11-14 11:40:25 +0300

    add signup endpoint

---
+++ fastify/server.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Form
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return { "Lux app":"Jambo Dunia" }
+
+@app.post("/signup/")
+async def signup(name : str = Form(...),country: str = Form(...),email: str = Form(...),username: str = Form(...),password: str = Form(...) ):
+    return { "name":name,"country":country,"email":email,"username":username, "password":password }
+app.post("/login/")
+async def login(username: str = Form(...), password: str = Form(...)):
+    return{ "username": username, "password": password}
+if __name__ == "__main__":
+    uvicorn.run('server:app', port=8000, reload=False)
